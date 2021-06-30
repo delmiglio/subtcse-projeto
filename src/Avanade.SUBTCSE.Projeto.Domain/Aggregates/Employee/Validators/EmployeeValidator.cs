f9c25565cedbf797d5ca1c69cf42d9f74fad2b77
Guilherme Oliveira Delmiglio <g.oliveira.delmiglio@avanade.com>
2021-06-30 07:58:23 -0300

    New Employee Role Validator

---
+++ src/Avanade.SUBTCSE.Projeto.Domain/Aggregates/Employee/Validators/EmployeeValidator.cs
@@ -0,0 +1,25 @@
+﻿using Avanade.SUBTCSE.Projeto.Domain.Aggregates.EmployeeRole.Validators;
+using FluentValidation;
+
+namespace Avanade.SUBTCSE.Projeto.Domain.Aggregates.Employee.Validators
+{
+    public class EmployeeValidator : AbstractValidator<Entities.Employee>
+    {
+        public EmployeeValidator()
+        {
+            RuleSet("new", () =>
+            {
+                RuleFor(e => e.FirstName)
+                .NotEmpty()
+                .WithMessage("{PropertyName} can not be empty");
+
+                RuleFor(e => e.LastName)
+                .NotEmpty()
+                .WithMessage("{PropertyName} can not be empty");
+
+                RuleFor(e => e.EmployeeRole)
+                .SetValidator(new EmployeeRoleValidator());
+            });
+        }
+    }
+}
