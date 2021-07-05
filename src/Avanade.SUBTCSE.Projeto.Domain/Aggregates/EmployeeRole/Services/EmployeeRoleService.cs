29d2a8a0c490d9bdfdf041e332dd6cd9fd1219f9
Guilherme Oliveira Delmiglio <g.oliveira.delmiglio@avanade.com>
2021-07-05 08:27:44 -0300

    Mongo Context

--- src/Avanade.SUBTCSE.Projeto.Domain/Aggregates/EmployeeRole/Services/EmployeeRoleService.cs
+++ src/Avanade.SUBTCSE.Projeto.Domain/Aggregates/EmployeeRole/Services/EmployeeRoleService.cs
@@ -5,11 +5,10 @@ namespace Avanade.SUBTCSE.Projeto.Domain.Aggregates.EmployeeRole.Services
 {
     public class EmployeeRoleService : IEmployeeRoleService
     {
-        public async Task<Entities.EmployeeRole> AddEmployeeRole(Entities.EmployeeRole employeeRole)
+        public Task<Entities.EmployeeRole> AddEmployeeRole(Entities.EmployeeRole employeeRole)
         {
             throw new System.NotImplementedException();
         }
 
-        // Fluent Val
     }
 }
---
+++ src/Avanade.SUBTCSE.Projeto.Domain/Base/Repository/MongoDb/IMongoDbContext.cs
@@ -0,0 +1,7 @@
+﻿namespace Avanade.SUBTCSE.Projeto.Domain.Base.Repository.MongoDb
+{
+    public interface IMongoDbContext
+    {
+
+    }
+}
